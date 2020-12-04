9c9d02c64e7792ce639930bea075c38b5463f625
Joe Phillips <phillijw@gmail.com>
2020-12-03 23:45:34 -0600

    Got the jekyll.sh working. Generated a new jekyll site

---
+++ jekyll.sh
@@ -0,0 +1,6 @@
+ docker run --rm \
+   -p 127.0.0.1:4000:4000/tcp \
+   --volume="$PWD:/srv/jekyll" \
+   --volume="$PWD/vendor/bundle:/usr/local/bundle" \
+   -it jekyll/jekyll:$JEKYLL_VERSION \
+   $@
