eb8dd26e4a7ba9d1f2970888f19eeaa1268a298f
Joe Phillips <phillijw@gmail.com>
2020-12-04 18:19:40 -0600

    Added the version to the script so I don't need to set it manually

--- jekyll.sh
+++ jekyll.sh
@@ -1,4 +1,5 @@
- docker run --rm \
+export JEKYLL_VERSION=3.8 
+docker run --rm \
    -p 127.0.0.1:4000:4000/tcp \
    --volume="$PWD:/srv/jekyll" \
    --volume="$PWD/vendor/bundle:/usr/local/bundle" \
